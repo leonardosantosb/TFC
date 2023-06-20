fa020e2f6c1df7442f5cfcf770af726cc0d71382
leonardosantosb <leonardosantosbasilio@gmail.com>
2023-06-20 16:37:35 -0300

    lint do mock

--- app/backend/src/mocks/mockMatches.ts
+++ app/backend/src/mocks/mockMatches.ts
@@ -1,40 +1,39 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 const allMatches = [
   {
     id: 1,
-    home_team_id: 16,
-    home_team_goals: 1,
-    away_team_id: 8,
-    away_team_goals: 1,
-    in_progress: false,
+    hometeamid: 16,
+    hometeamgoals: 1,
+    awayteamid: 8,
+    awayteamgoals: 1,
+    inprogress: false,
   },
   {
     id: 2,
-    home_team_id: 9,
-    home_team_goals: 1,
-    away_team_id: 14,
-    away_team_goals: 1,
-    in_progress: false,
+    hometeamid: 9,
+    hometeamgoals: 1,
+    awayteamid: 14,
+    awayteamgoals: 1,
+    inprogress: false,
   },
 ];
 
 const matchesEnded = [
   {
     id: 3,
-    home_team_id: 16,
-    home_team_goals: 1,
-    away_team_id: 8,
-    away_team_goals: 1,
-    in_progress: false,
+    hometeamid: 16,
+    hometeamgoals: 1,
+    awayteamid: 8,
+    awayteamgoals: 1,
+    inprogress: false,
   }];
 
 const createMatches = [
   {
-    home_team_id: 4,
-    home_team_goals: 3,
-    away_team_id: 11,
-    away_team_goals: 0,
-    in_progress: true,
+    hometeamid: 4,
+    hometeamgoals: 3,
+    awayteamid: 11,
+    awayteamgoals: 0,
+    inprogress: true,
   },
 ];
 
